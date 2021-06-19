43ffe3bbbbd2efc63f7a639791fbcd777d6805c9
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-06-19 13:23:49 -0700

    rename image

--- secCamRun.sh
+++ secCamRun.sh
@@ -1,12 +1,8 @@
 flutter clean && \
 flutter build web && \
-# docker stop movflo && \
-# docker rm movflo && \
 docker-compose down && \
 docker build -t seccamhttpdimg . && \
 docker-compose up -d --build && \
-# docker run -dit --name movflo -p 7777:80 movflohttpdimg && \
-
 snapcraft --use-lxd && \
-sudo snap install --dangerous movflo*snap
+sudo snap install --dangerous sec*snap
 
